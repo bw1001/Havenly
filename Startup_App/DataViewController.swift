b5f5eb732042b427f56888b99a1bcc5ca2653cb5
Clayton Kingsbury <ckingsbury2@gmail.com>
2018-07-06 15:51:57 -0700

    Initial Commit

---
+++ Startup_App/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  Startup_App
+//
+//  Created by Clayton kingsbury on 7/6/18.
+//  Copyright © 2018 Clayton kingsbury. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
