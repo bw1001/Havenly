ab2b70715f63cf98d15ddb30d213d17e9e8ecb6b
Clayton Kingsbury <ckingsbury2@gmail.com>
2018-07-09 22:39:06 -0700

    server start

---
+++ App_Server/App_Server/App_Server.h
@@ -0,0 +1,19 @@
+//
+//  App_Server.h
+//  App_Server
+//
+//  Created by Clayton kingsbury on 7/9/18.
+//  Copyright © 2018 Clayton kingsbury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for App_Server.
+FOUNDATION_EXPORT double App_ServerVersionNumber;
+
+//! Project version string for App_Server.
+FOUNDATION_EXPORT const unsigned char App_ServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <App_Server/PublicHeader.h>
+
+
---
+++ App_Server/App_Server/Server/server.swift
@@ -0,0 +1,9 @@
+//
+//  server.swift
+//  App_Server
+//
+//  Created by Clayton kingsbury on 7/9/18.
+//  Copyright © 2018 Clayton kingsbury. All rights reserved.
+//
+
+import Foundation
