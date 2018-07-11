cd04249bbe9d50eb2784d92ac1732d3f0f5db9be
Clayton Kingsbury <ckingsbury2@gmail.com>
2018-07-10 19:25:37 -0700

    networking

--- Startup_App/UI_Stuff/LoginScreen/Network/contactServer.swift
+++ Startup_App/UI_Stuff/LoginScreen/Network/contactServer.swift
@@ -9,50 +9,66 @@
 import Foundation
 
 class contactServer{
-    func Contact(info: [Any]){
+    func Contact(info: [infoToSend]) -> Any{
         //var post:String = "username=\(username)&password=\(password)"
-        //
-        //            NSLog("PostData: %@",post);
-        //
-        //            var url:NSURL = NSURL(string: "https://192.168.1.35/8080/")! // this is used to contact server?
-        //
-        //            var postData:Data = post.data(using: String.Encoding.ascii)!
-        //
-        //            var postLength:String = String( postData.count )
-        //
-        //            var request:NSMutableURLRequest = NSMutableURLRequest(url: url as URL)
-        //            request.httpMethod = "POST"
-        //            request.httpBody = postData
-        //            request.setValue(postLength, forHTTPHeaderField: "Content-Length")
-        //            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        //            request.setValue("application/json", forHTTPHeaderField: "Accept")
-        //
-        //
-        //            var reponseError: NSError?
-        //            var response: URLResponse?
-        //
-        //            var urlData: NSData? = NSURLConnection.sendSynchronousRequest(request, returningResponse:&response, error:&reponseError)
-        //
-        //            if ( urlData != nil ) {
-        //                let res = response as! HTTPURLResponse!;
-        //
-        //                NSLog("Response code: %ld", (res?.statusCode)!);
-        //
-        //                if ((res?.statusCode)! >= 200 && (res?.statusCode)! < 300)
-        //                {
-        //                    var responseData:NSString  = NSString(data:urlData! as Data, encoding:String.Encoding.utf8.rawValue)!
-        //
-        //                    NSLog("Response ==> %@", responseData);
-        //
-        //                    var error: NSError?
-        //
-        //                    let jsonData:NSDictionary = JSONSerialization.JSONObjectWithData(urlData!, options:JSONSerialization.ReadingOptions.MutableContainers , error: &error) as NSDictionary
-        //
-        //
-        //                    let success:NSInteger = jsonData.value(forKey: "success") as! NSInteger
-        //
-        //                    //[jsonData[@"success"] integerValue];
-        //
-        //                    NSLog("Success: %ld", success);
+        
+           // NSLog("PostData: %@",post);
+
+        var url:NSURL = NSURL(string: "https://192.168.1.35/8080/")! // this is used to contact server?
+
+            //var postData:Data = post.data(using: String.Encoding.ascii)!
+
+           // var postLength:String = String( postData.count )
+
+        var request:NSMutableURLRequest = NSMutableURLRequest(url: url as URL)
+        request.httpMethod = "POST"
+            //request.httpBody = postData
+
+        for object in info {
+            request.setValue(object.information, forHTTPHeaderField: object.label)
+        }
+
+        
+
+
+        var reponseError: NSError?
+        var response: URLResponse?
+        
+        do {
+            var urlData: NSData? = try NSURLConnection.sendSynchronousRequest(request as URLRequest, returning:&response) as NSData
+            if ( urlData != nil ) {
+                let res = response as! HTTPURLResponse!;
+                
+                NSLog("Response code: %ld", (res?.statusCode)!);
+                
+                if ((res?.statusCode)! >= 200 && (res?.statusCode)! < 300)
+                {
+                    var responseData:NSString  = NSString(data:urlData! as Data, encoding:String.Encoding.utf8.rawValue)!
+                    
+                    NSLog("Response ==> %@", responseData);
+                    
+                    var error: NSError?
+                    do {
+                        let jsonData:NSDictionary = try JSONSerialization.jsonObject(with: urlData! as Data, options:JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                        let success:NSInteger = jsonData.value(forKey: "success") as! NSInteger
+                        
+                        //[jsonData[@"success"] integerValue];
+                        
+                        NSLog("Success: %ld", success);
+                        return jsonData
+                    }
+                    catch{
+                        print("Error in Networking 2")
+                    }
+                    
+                    
+                }
+            }
+        }
+        catch{
+            print("Error in networking 2")
+        }
+        return "Error"
+        
     }
 }
---
+++ Startup_App/UI_Stuff/LoginScreen/Network/infoToSend.swift
@@ -0,0 +1,20 @@
+//
+//  infoToSend.swift
+//  Startup_App
+//
+//  Created by Clayton kingsbury on 7/10/18.
+//  Copyright © 2018 Clayton kingsbury. All rights reserved.
+//
+
+import Foundation
+class infoToSend{
+    var  label: String
+    var information: String
+    public init(label: String, info: String){
+        self.label = label
+        self.information = info
+    }
+    public func retEncrypted(){
+        // return the encrypted infromation
+    }
+}
